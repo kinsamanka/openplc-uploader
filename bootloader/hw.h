9f8c76a6d7346942f2a71192da2ceadf598c8bb5
GP Orcullo <kinsamanka@gmail.com>
2023-01-13 07:26:49 +0800

    add stm32f1xx uart bootloader

---
+++ bootloader/hw.h
@@ -0,0 +1,36 @@
+#ifndef HW_H
+#define HW_H
+
+#include <stdint.h>
+
+struct gpios {
+    const uint32_t port;
+    const uint16_t pin;
+};
+
+struct leds {
+    const struct gpios gpio;
+    const int inv;
+};
+
+struct uarts {
+    const uint8_t index;
+    const uint32_t port;
+    const uint16_t rx;
+    const uint16_t tx;
+    const uint32_t remaps;
+    const struct gpios en;
+};
+
+void adc_setup(void);
+void adc_start(void);
+void clock_setup(void);
+void dac_setup(void);
+void gpio_setup(void);
+void run_bootloader(void);
+void spi_setup(void);
+void timer3_setup(void);
+void update_inputs(void);
+void update_ouputs(void);
+
+#endif
